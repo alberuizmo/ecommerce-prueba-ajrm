319293b42c51fdb460cee0ab98e63de65f91f9c7
alberuizmo <albertojruizm@gmail.com>
2025-03-19 17:06:59 -0500

    z index

--- packages/ecommerce-utils-ajrm/src/Toast.tsx
+++ packages/ecommerce-utils-ajrm/src/Toast.tsx
@@ -22,7 +22,8 @@ const Toast: FC<ToastProps> = ({ message, type = "success", onClose }) => {
     <div      
       className={`fixed top-20 right-5 px-4 py-2 rounded-lg shadow-lg text-white text-md transition-opacity duration-400 ${
         type === "success" ? "bg-green-500" : "bg-red-500"
-      } z-50`}
+      }`}
+      style={{ zIndex: 999 }}
     >
       {message}
     </div>
